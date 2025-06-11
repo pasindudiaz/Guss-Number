90fc7ded69a420b1bd874ce5725b5d02403437ea
pasindudiaz <workspaceuseonly@gmail.com>
2025-06-11 10:22:17 +0530

    reset btn number generate

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ function clickAction() {
 function resetGame() {
     attempts = 4;
     document.getElementById("resultMsg").innerText = "Good Luck Buddy";
+    randomnumber = Math.floor(Math.random() * 10) + 1;
+    console.log(randomnumber);
 }
 
 
